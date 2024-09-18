2b047cc62b1a22ec8397934f9dc75996b26cc9ee
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-09-18 15:17:55 +0500

    finito

--- lab_2.c
+++ lab_2.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 void main(){
+// 2. What does the following code print? printf( "*\n**\n***\n****\n*****\n" );2. What does the following code print?
+ printf( "*\n**\n***\n****\n*****\n" );
 
+//  3. 
+printf("*\n*\t*\n*\t*\t*\n*\t*\t*\t*\n");
+printf("*\n*\t\t*\n*\t\t*\t\t*\n*\t\t*\n*");
+
+
+// square
+ printf("*************\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*************\n");     
+
+
+// arrow
 printf("  *\n *** \n*****\n  *\n  *\n  *\n  *\n  *\n  *\n  * \n\n");
 
+//somewhat oval
+printf("    ***********\n  *\t\t*\n *\t\t *\n *\t\t *\n *\t\t *\n  *\t\t*\n    ***********\n");
+
+// diamond
 printf("      *\n    *   *\n   *     *\n  *       *\n *         *\n*           *\n *         *\n  *       *\n   *     *\n    *   *\n      *\n");
 
 }
